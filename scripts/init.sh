1ce56b43716eb1542cb0245ff83b1cca91f8a485
aoikurokawa <ht.so.ik0102@gmail.com>
2025-10-16 17:14:03 +0900

    fix: update

---
+++ scripts/init.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+
+# Stakenet
+
+# Validator History
+
+# Program ID
+solana account HistoryJTGbKQD2mRgLZ3XhqHnN811Qpez8X9kCcGHoa --url http://127.0.0.1:8899
+# Upgrade Authority
+solana account 5eosrve6LktMZgVNszYzebgmmC7BjLK8NoWyRQtcmGTF --url http://127.0.0.1:8899
+# IDL Authority
+solana account 9eZbWiHsPRsxLSiHxzg2pkXsAuQMwAjQrda7C7e21Fw6 --url http://127.0.0.1:8899
+# History Config
+solana account BZP2F1L4FwCaeTPmCmfGiVJAiMFwGawo5mbLj4JFzwEd --url http://127.0.0.1:8899
+# Cranker
+solana account CRnkKQTxctQ7LHVN3yssdgJyEksBJeBrDdwZAxBtsJoZ --url http://127.0.0.1:8899
+
+# Steward
+
+# Program ID
+solana account Stewardf95sJbmtcZsyagb2dg4Mo8eVQho8gpECvLx8 --url http://127.0.0.1:8899
+# Upgrade Authority
+solana account 5eosrve6LktMZgVNszYzebgmmC7BjLK8NoWyRQtcmGTF --url http://127.0.0.1:8899
+# IDL Authority
+solana account 9eZbWiHsPRsxLSiHxzg2pkXsAuQMwAjQrda7C7e21Fw6 --url http://127.0.0.1:8899
+# Steward Config
+solana account jitoVjT9jRUyeXHzvCwzPgHj7yWNRhLcUoXtes4wtjv --url http://127.0.0.1:8899
+# Steward state
+solana account 9BAmGVLGxzqct6bkgjWmKSv3BFB6iKYXNBQp8GWG1LDY --url http://127.0.0.1:8899
+
+
+# Stake Pool
+
+# Program ID
+solana account SPoo1Ku8WFXoNDMHPsrGSTSG1Y47rzgn41SLUNakuHy --url http://127.0.0.1:8899
+# Stake Pool
+solana account Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb --url http://127.0.0.1:8899
+# Reserve Stake
+solana account BgKUXdS29YcHCFrPm5M8oLHiTzZaMDjsebggjoaQ6KFL --url http://127.0.0.1:8899
+# Pool mint
+solana account J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn --url http://127.0.0.1:8899
+
+
+# Tip Payment
+#
+# Program ID
+solana account 4R3gSG8BpU4t19KYj8CfnbtRpnT8gtk4dvTHxVRwc2r7 --url http://127.0.0.1:8899
+
+
+# Tip Distribution
+#
+# Program ID
+solana account T1pyyaTNZsKv2WcRAB8oVnk93mLJw2XzjtVYqCsaHqt --url http://127.0.0.1:8899
